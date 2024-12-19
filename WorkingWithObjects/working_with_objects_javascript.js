81dda57a4166e342ba32a7c55acc67a2f1346e03
valyukhov_dn <sibtorg2002@yandex.ru>
2024-12-19 19:00:33 +0700

    Корректировка кода задачи "Working with objects" с учётом замечаний от 17.12.2024. В задаче "Working with arrays" текст на странице и title сделал на русском.

--- WorkingWithObjects/working_with_objects_javascript.js
+++ WorkingWithObjects/working_with_objects_javascript.js
@@ -1,109 +1,143 @@
 (function () {
-    "use strict"
+    "use strict";
 
-    const russia = {
-        countryName: "Россия",
-        cities: [
-            {"Москва": 13010112},
-            {"Санкт-Петербург": 5601911},
-            {"Новосибирск": 1633595},
-            {"Екатеринбург": 1588665},
-            {"Казань": 1308660},
-            {"Нижний Новгород": 1249861},
-            {"Челябинск": 1189525},
-            {"Красноярск": 1188533},
-            {"Самара": 1173393},
-            {"Уфа": 1144809}
-        ]
-    };
+    const countries = [
+        {
+            name: "Россия",
+            cities: [
+                {name: "Москва", population: 13010112},
+                {name: "Санкт-Петербург", population: 5601911},
+                {name: "Новосибирск", population: 1633595},
+                {name: "Екатеринбург", population: 1588665},
+                {name: "Казань", population: 1308660},
+                {name: "Нижний Новгород", population: 1249861},
+                {name: "Челябинск", population: 1189525},
+                {name: "Красноярск", population: 1188533},
+                {name: "Самара", population: 1173393},
+                {name: "Уфа", population: 1144809},
+            ]
+        },
 
-    const belarus = {
-        countryName: "Беларусь",
-        cities: [
-            {"Минск": 1992900},
-            {"Гомель": 501100},
-            {"Гродно": 361100},
-            {"Витебск": 358400},
-            {"Могилёв": 353100}
-        ]
-    };
+        {
+            name: "Беларусь",
+            cities: [
+                {name: "Минск", population: 1992900},
+                {name: "Гомель", population: 501100},
+                {name: "Гродно", population: 361100},
+                {name: "Витебск", population: 358400},
+                {name: "Могилёв", population: 353100},
+            ]
+        },
 
-    const germany = {
-        countryName: "Германия",
-        cities: [
-            {"Берлин": 3677472},
-            {"Гамбург": 1853935},
-            {"Мюнхен": 1487708},
-            {"Кёльн": 1073096},
-            {"Франкфурт-на-Майне": 759224},
-            {"Штутгарт": 626275},
-            {"Дюссельдорф": 619477}
-        ]
-    };
+        {
+            name: "Германия",
+            cities: [
+                {name: "Берлин", population: 3677472},
+                {name: "Гамбург", population: 1853935},
+                {name: "Мюнхен", population: 1487708},
+                {name: "Кёльн", population: 1073096},
+                {name: "Франкфурт-на-Майне", population: 759224},
+                {name: "Штутгарт", population: 626275},
+                {name: "Дюссельдорф", population: 619477},
+            ]
+        },
 
-    const belgium = {
-        countryName: "Бельгия",
-        cities: [
-            {"Антверпен": 472526},
-            {"Гент": 233120},
-            {"Шарлеруа": 201300},
-            {"Льеж": 186805}
-        ]
-    };
+        {
+            name: "Бельгия",
+            cities: [
+                {name: "Антверпен", population: 472526},
+                {name: "Гент", population: 233120},
+                {name: "Шарлеруа", population: 201300},
+                {name: "Льеж", population: 186805},
+            ]
+        },
 
-    const france = {
-        countryName: "Франция",
-        cities: [
-            {"Париж": 2249975},
-            {"Марсель": 850636},
-            {"Лион": 491268},
-            {"Тулуза": 447360},
-            {"Ницца": 344068},
-            {"Нант": 287845},
-            {"Страсбург": 272222},
-            {"Монпелье": 246538}
-        ]
-    };
+        {
+            name: "Франция",
+            cities: [
+                {name: "Париж", population: 2249975},
+                {name: "Марсель", population: 850636},
+                {name: "Лион", population: 491268},
+                {name: "Тулуза", population: 447360},
+                {name: "Ницца", population: 344068},
+                {name: "Нант", population: 287845},
+                {name: "Страсбург", population: 272222},
+                {name: "Монпелье", population: 246538}, // «Висячая запятая» упрощает добавление, удаление и перемещение свойств, так как все строки объекта становятся одинаковыми.
+            ]
+        },
+    ];
 
-// Создание массива объектов-стран
-    const countries = [russia, belarus, germany, belgium, france];
-
-// Нахождение страны с максимальным количеством городов
-    function findCountryWithMaxNumberOfCities(countries) {
-        if (typeof countries !== "object" || countries == null) {
-            console.error("Попытка передать некорректный аргумент в функцию нахождения страны с максимальным количеством городов.")
-            return;
+    // Нахождение страны с максимальным количеством городов
+    function getCountriesWithMaxCitiesQuantity(countries) {
+        if (countries == null) {
+            throw new Error("null вместо массива передан в качестве аргумента в метод нахождения страны с максимальным количеством городов.");
         }
 
-        countries.sort((e1, e2) => e2.cities.length - e1.cities.length);
-
-        return countries[0];
-    }
-
-    console.log("Страна с максимальным количеством городов: " + findCountryWithMaxNumberOfCities(countries).countryName + " (кол-во городов: "
-        + findCountryWithMaxNumberOfCities(countries).cities.length + `).
-        
-        `);
-
-// Получение объекта с информацией по всем странам такого вида: ключ – название страны, значение – суммарная численность по стране
-    function createObjectConsistingOfCountryNamesAndPopulationSizes(countries) {
-        if (typeof countries !== "object" || countries == null) {
-            console.error("Попытка передать некорректный аргумент в функцию создания объекта с названиями стран и численностью населения в них.")
-            return;
+        if (!Array.isArray(countries)) {
+            throw new Error("Не массив вместо массива передан в качестве аргумента в метод нахождения страны с максимальным количеством городов.");
         }
 
-        const objectConsistingOfCountryNamesAndPopulationSizes = [];
+        if (countries.length < 1) {
+            throw new Error("Пустой массив (должен быть хотя бы один элемент) передан в качестве аргумента в метод нахождения страны с максимальным количеством городов.")
+        }
 
-        countries.forEach(country => objectConsistingOfCountryNamesAndPopulationSizes[Object.values(country)[0]] = country.cities.reduce((city1, city2) => Number(Object.values(city1)) + Number(Object.values(city2)), 0));
+        countries.sort((country1, country2) => country2.cities.length - country1.cities.length);
 
-        return objectConsistingOfCountryNamesAndPopulationSizes;
+        const countriesWithMaxCitiesQuantity = [countries[0]];
+
+        const maxCitiesQuantity = countries[0].cities.length;
+
+        for (let i = 1; i < countries.length; ++i) {
+            if (countries[i].cities.length === maxCitiesQuantity) {
+                countriesWithMaxCitiesQuantity.push(countries[i]);
+            }
+        }
+
+        return countriesWithMaxCitiesQuantity;
     }
 
-    const object = createObjectConsistingOfCountryNamesAndPopulationSizes(countries);
+    const countriesWithMaxCitiesQuantity = getCountriesWithMaxCitiesQuantity(countries);
+
+    console.log("Страны с максимальным количеством городов: ");
+
+    for (const country of countriesWithMaxCitiesQuantity) {
+        console.log(country.name + " (кол-во городов: " + country.cities.length + ")");
+    }
+
+    console.log("");
+
+    // Получение объекта с информацией по всем странам такого вида: ключ – название страны, значение – суммарная численность по стране
+    function getCountryNamesWithPopulationSizes(countries) {
+        if (countries == null) {
+            throw new Error("null вместо массива передан в качестве аргумента в метод создания объекта с названиями стран и численностью населения в них.");
+        }
+
+        if (!Array.isArray(countries)) {
+            throw new Error("Не массив вместо массива передан в качестве аргумента в метод создания объекта с названиями стран и численностью населения в них.");
+        }
+
+        if (countries.length < 1) {
+            throw new Error("Пустой массив (должен быть хотя бы один элемент) передан в качестве аргумента в метод создания объекта с названиями стран и численностью населения в них.")
+        }
+
+        let countryNamesWithPopulationSizes = {};
+
+        countries.forEach(country => {
+            let population = 0;
+
+            country.cities.forEach(city => population += city.population);
+
+            countryNamesWithPopulationSizes[country.name] = population;
+        });
+
+        return countryNamesWithPopulationSizes;
+    }
+
+    const countryNamesWithPopulationSizes = getCountryNamesWithPopulationSizes(countries);
 
     console.log("Содержимое полученного объекта с информацией по всем странам вида: ключ – название страны, значение – численность населения по стране:");
 
-    for (const country in object) {
-        console.log("%s   %d", country, object[country]);
+    for (const countryName in countryNamesWithPopulationSizes) {
+        console.log("%s   %d", countryName, countryNamesWithPopulationSizes[countryName]);
     }
 })();
